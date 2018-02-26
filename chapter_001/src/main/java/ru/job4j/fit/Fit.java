c4a29e5aa01d7fb2afd79bd372c7c65bb4c57450
Layton85 <alex-programm@yandex.ru>
2018-02-26 17:36:50 +0300

    3.3. Идеальный вес. [#41584]

---
+++ chapter_001/src/main/java/ru/job4j/fit/Fit.java
@@ -0,0 +1,30 @@
+package ru.job4j.calculator;
+
+/**
+ * Программа расчета идеального веса
+ * @author Alexander Kordyukov (alex-programm@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Fit {
+
+	/**
+	* Идеальный вес для мужчины
+	* @param height рост
+	* @return weight вес
+	*/
+    double manWeight(double height) {
+        double weight = (height - 100)*1.15;
+		return weight;
+    }
+
+	/**
+	* Идеальный вес для женщины
+	* @param height рост
+	* @return weight вес
+	*/
+    double womanWeight(double height) {
+        double weight = (height - 100)*1.15;
+		return weight;
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/fit/FitTest.java
@@ -0,0 +1,30 @@
+﻿package ru.job4j.calculator;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Alexander Kordyukov (alex-programm@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+public class FitTest {
+
+    @Test
+    public void manWeight() {
+        Fit fit = new Fit();
+        double weight = fit.manWeight(180);
+        assertThat(weight, is(92D));
+    }
+
+    @Test
+    public void womanWeight() {
+        Fit fit = new Fit();
+        double weight = fit.manWeight(170);
+        assertThat(weight, is(69D));
+    }
+}
