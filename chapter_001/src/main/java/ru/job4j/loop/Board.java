cc95536c8855a3b145ad7ea80f787867bd1d95cd
Layton85 <alex-programm@yandex.ru>
2018-04-04 18:24:39 +0300

    4.3. Построить шахматную доску в псевдографике.

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,34 @@
+package ru.job4j.loop;
+
+/**
+ * Class chessboard.
+ *
+ * @author Alexander Kordyukov (alex-programm@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Board {
+    /**
+     * The method draws a chessboard in a pseudo-graphic
+     * @param width number of cells in a chessboard in width
+     * @param height number of cells in a chessboard in height
+     * @return chessboard as a string
+     */
+    public String paint(int width, int height) {
+        StringBuilder screen = new StringBuilder();
+        String ln = System.lineSeparator();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                //X - если сумма номера столбца и строки - четное число
+                if ((i+j) % 2 == 0) {
+                    screen.append("X");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            // добавляем перевод на новую строку.
+            screen.append(ln);
+        }
+        return screen.toString();
+    }
+}
