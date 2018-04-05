bce1b33811b57876507dbf7ebb7e8dd2446bd103
Layton85 <alex-programm@yandex.ru>
2018-04-05 17:34:29 +0300

    4.5. Рефакторинг кода - убрал закомментированный код.

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -49,26 +49,3 @@ public class Paint {
         return screen.toString();
     }
 }
-
-//public class Paint {
-//    /**
-//     * The method draws a chessboard in a pseudo-graphic
-//     * @param height number of levels in a pyramid
-//     * @return pyramid as a string
-//     */
-//    public String pyramid(int height) {
-//        StringBuilder screen = new StringBuilder();
-//        int weight = 2 * height - 1;
-//        for (int row = 0; row != height; row++) {
-//            for (int column = 0; column != weight; column++) {
-//                if (row >= height - column - 1 && row + height - 1 >= column) {
-//                    screen.append("^");
-//                } else {
-//                    screen.append(" ");
-//                }
-//            }
-//            screen.append(System.lineSeparator());
-//        }
-//        return screen.toString();
-//    }
-//}
