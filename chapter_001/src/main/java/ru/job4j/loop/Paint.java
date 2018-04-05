fca4af075182a530569aca4c65c6edaabceb888f
Layton85 <alex-programm@yandex.ru>
2018-04-05 17:25:34 +0300

    4.5. Рефакторинг кода.

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,5 +1,7 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
+
 /**
  * Class for drawing in pseudo-graphic.
  *
@@ -8,17 +10,35 @@ package ru.job4j.loop;
  * @since 0.1
  */
 public class Paint {
-    /**
-     * The method draws a chessboard in a pseudo-graphic
-     * @param height number of levels in a pyramid
-     * @return pyramid as a string
-     */
+    public String rightTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= column
+        );
+    }
+
+    public String leftTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= height - column - 1
+        );
+    }
+
     public String pyramid(int height) {
+        return this.loopBy(
+                height,
+                2 * height - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+        );
+    }
+
+    private String loopBy(int height, int weight, BiPredicate<Integer, Integer> predict) {
         StringBuilder screen = new StringBuilder();
-        int weight = 2 * height - 1;
         for (int row = 0; row != height; row++) {
             for (int column = 0; column != weight; column++) {
-                if (row >= height - column - 1 && row + height - 1 >= column) {
+                if (predict.test(row, column)) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
@@ -29,3 +49,26 @@ public class Paint {
         return screen.toString();
     }
 }
+
+//public class Paint {
+//    /**
+//     * The method draws a chessboard in a pseudo-graphic
+//     * @param height number of levels in a pyramid
+//     * @return pyramid as a string
+//     */
+//    public String pyramid(int height) {
+//        StringBuilder screen = new StringBuilder();
+//        int weight = 2 * height - 1;
+//        for (int row = 0; row != height; row++) {
+//            for (int column = 0; column != weight; column++) {
+//                if (row >= height - column - 1 && row + height - 1 >= column) {
+//                    screen.append("^");
+//                } else {
+//                    screen.append(" ");
+//                }
+//            }
+//            screen.append(System.lineSeparator());
+//        }
+//        return screen.toString();
+//    }
+//}
