3866003804a969e1f9a00d7765e8eeb06faad2e2
Layton85 <alex-programm@yandex.ru>
2018-04-13 17:57:06 +0300

    5.1. Классический поиск перебором.

---
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+
+/**
+ * FindLoop
+ *
+ * @author Alexander Kordyukov (alex-programm@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class FindLoop {
+    /**
+     * The method searches the array for a given value of the element by simply enumerating
+     * @param data array
+     * @param el value of the element that is searched in the array
+     * @return index of the found element
+     */
+    public int indexOf(int[] data, int el) {
+        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
+        for (int index = 0 ; index != data.length; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * FindLoopTest.
+ *
+ * @author Alexander Kordyukov (alex-programm@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class FindLoopTest {
+
+    @Test
+    public void whenArrayHasLengh5Then0() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 10, 3};
+        int value = 5;
+        int result = find.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenArrayHasLengh3Then2() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 10, 3};
+        int value = 3;
+        int result = find.indexOf(input, value);
+        int expect = 2;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenArrayHasLengh4ThenMinus1() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 10, 3};
+        int value = 4;
+        int result = find.indexOf(input, value);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+}
