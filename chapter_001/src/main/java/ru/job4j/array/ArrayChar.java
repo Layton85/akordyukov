ba4095807c4b454eb8d241c9331eb9184382a8e1
Layton85 <alex-programm@yandex.ru>
2018-05-07 14:07:14 +0300

    5.5. Слова начинается с ... [#41585]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,40 @@
+package ru.job4j.array;
+
+/**
+ * ArrayChar - class cover over the string.
+ *
+ * @author Alexander Kordyukov (alex-programm@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class ArrayChar {
+    /**
+     * Char array initialized with the initializing string.
+     */
+    private char[] data;
+
+    /**
+     * Constructor.
+     * @param initializing string.
+     */
+    public ArrayChar(String line) {
+        this.data = line.toCharArray();
+    }
+
+    /**
+     * Checks that the word begins with a prefix.
+     * @param prefix .
+     * @return true - if the line begin with the prefix, false - if not.
+     */
+    public boolean startWith(String prefix) {
+        boolean result = true;
+        char[] value = prefix.toCharArray();
+        for (int i = 0; i < value.length; i++) {
+            if (value[i] != data[i]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
