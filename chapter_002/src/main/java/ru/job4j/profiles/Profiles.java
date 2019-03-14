984331fdf914823389edceab4f4a51280dc9d6aa
Layton85 <alex-programm@yandex.ru>
2019-03-14 12:05:38 +0300

    004.FP,Lambda,Stream API. 2.Stream API. 3.Уникальность элементов и сортировка. Испр.1.

--- chapter_002/src/main/java/ru/job4j/profiles/Profiles.java
+++ chapter_002/src/main/java/ru/job4j/profiles/Profiles.java
@@ -17,11 +17,11 @@ public class Profiles {
      * @return - List of their addresses.
      */
     public List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map((profile) -> new Address(profile.getAddress())).distinct().sorted(new Comparator<Address>() {
+        return profiles.stream().map((profile) -> new Address(profile.getAddress())).sorted(new Comparator<Address>() {
             @Override
             public int compare(Address o1, Address o2) {
                 return o1.getCity().compareTo(o2.getCity());
             }
-        }).collect(Collectors.toList());
+        }).distinct().collect(Collectors.toList());
     }
 }
